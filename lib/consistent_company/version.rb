66ed3be9eb61fc0926d15daf866375ebd238de88
Doug Cleven <dcleven@marketron.com>
2011-09-15 09:03:30 -0600

    add missing version.rb

---
+++ lib/consistent_company/version.rb
@@ -0,0 +1,10 @@
+module ConsistentCompany
+  module Version
+     MAJOR = 0
+     MINOR = 0
+     PATCH = 3
+     BUILD = nil
+
+     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
+   end
+end
